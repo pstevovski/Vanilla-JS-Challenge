0fd1815c1a54e00d9b4d758e5b747e0098e812a7
Petar Stevovski <stevovski.petar@gmail.com>
2018-09-15 16:14:58 +0200

    Day 22 - Sticky Nav.

---
+++ Day 22 - Sticky Nav/main.js
@@ -0,0 +1,13 @@
+const nav = document.querySelector("#main");
+const topOfNav = nav.offsetTop;
+function fixNav(){
+    if(window.scrollY >= topOfNav) {
+        document.body.style.paddingTop = nav.offsestHeight + "px";
+        document.body.classList.add("fixedNav");
+    } else {
+        document.body.style.paddingTop = 0;
+        document.body.classList.remove("fixedNav");
+    }
+}
+
+window.addEventListener('scroll', fixNav);
