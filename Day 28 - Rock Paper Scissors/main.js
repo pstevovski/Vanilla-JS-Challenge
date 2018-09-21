b9d308b4aecd926996e1b1b89fc681c8478c4318
Petar Stevovski <stevovski.petar@gmail.com>
2018-09-21 23:13:58 +0200

    Day 28 - Rock Paper Scissors game.

--- Day 28 - Rock Paper Scissors/main.js
+++ Day 28 - Rock Paper Scissors/main.js
@@ -6,43 +6,62 @@ const countdownTimer = document.querySelector(".countdown");
 const playerOption = document.querySelector(".player_option");
 const computer = document.querySelector(".computer_option");
 const option = document.querySelectorAll(".option");
+const playerScoreOutput = document.querySelector(".player_score");
+const computerScoreOutput = document.querySelector(".computer_score");
+let playerScore = 0;
+let computerScore = 0;
 
-// Start the game
+// Start the game (hide the menues).
 document.querySelector("#startGame").addEventListener("click", ()=>{
     document.querySelector(".intro_menu").style.display = "none";
     document.querySelector(".choose").style.display = "block";
 })
 
-// // Rock options
-// rock.addEventListener('click', ()=>{
-//     playerOption.innerHTML = `<img src="rock.svg" width="60px" height="60px">`;
-// })
-// // Paper option
-// paper.addEventListener("click", ()=>{
-//     playerOption.innerHTML = `<img src="scroll.svg" width="60px" height="60px">`
-// })
-// // Scissors option
-// scissors.addEventListener("click", ()=>{
-//     playerOption.innerHTML = `<img src="scissors.svg" width="60px" height="60px">`;
-// })
-
-
+// Get the choice selected by the player.
 function updatePlayerOption(){
+    document.querySelector(".game").style.display = "flex";
     const playerChoice = this.dataset.option;
-    playerOption.innerHTML = `<img src="${playerChoice}.svg" width="60px" height="60p">`;
+    playerOption.innerHTML = `<img src="${playerChoice}.svg" width="70px" height="70px">`;
     
-    // Store the selected option in session storage.
+    // Store the player option in session storage.
     sessionStorage.setItem("playerOption", playerChoice);
+    computerOption();
 }
 
+// Get a randomly selected choice by the computer.
 function computerOption(){
-    const computerOptions = ["rock", "scroll", "scissors"];
+    const computerOptions = ["rock", "paper", "scissors"];
     const computerChoice = Math.floor(Math.random() * computerOptions.length);
-    computer.innerHTML = `<img src="${computerOptions[computerChoice]}.svg" width="60px" height="60px">`;
+    computer.innerHTML = `<img src="${computerOptions[computerChoice]}.svg" width="70px" height="70px">`;
 
     // Store the computer option in session storage.
     sessionStorage.setItem("computerOption", computerOptions[computerChoice]);
-    console.log(computerOptions[computerChoice]);
+    compare();
 }
 
+// Compare the chosen options between the player and the computer.
+function compare(){
+    // Get the choices from the session storage.
+    const playerChoice = sessionStorage.getItem("playerOption");
+    const computerChoice = sessionStorage.getItem("computerOption");
+
+    // Player wins if the conditions are met.
+    if(playerChoice == "rock" && computerChoice == "scissors" || playerChoice == "scissors" && computerChoice == "paper" || playerChoice == "paper" && computerChoice == "rock") {
+        playerScore++;
+        playerScoreOutput.innerHTML = "Player: "+playerScore;
+    }
+    // Computer wins if the conditions are met.
+    else if (playerChoice == "rock" && computerChoice == "paper" || playerChoice == "scissors" && computerChoice == "rock" || playerChoice == "paper" && computerChoice == "scissors") {
+        computerScore++;
+        computerScoreOutput.innerHTML = "Computer: "+computerScore;
+    }
+    // If choices are the same, its a draw, resulting in the computer making another choice.
+    else if (playerChoice == computerChoice) {
+       return computerOption();
+    }
+}
+
+// Add event listeners to the choices buttons.
 option.forEach(option => option.addEventListener("click", updatePlayerOption))
+
+// TODO : Change the if statements with a Hash Table or a Switch / case option once I get a better understanding of these.
