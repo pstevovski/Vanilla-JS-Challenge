40aefc0cdb461bdd30dd650e80f3b1c495086cea
Petar Stevovski <stevovski.petar@gmail.com>
2018-09-25 02:08:38 +0200

    Day - 30 JavaScript cardio.

--- Day 30 - JavaScript cardio/index.js
+++ Day 30 - JavaScript cardio/index.js
@@ -1,8 +1,8 @@
 // CHALLENGE 1: Reverse a string.
-// function reverseString(str){
+function reverseString(str){
     // METHOD 1:
 
-    // return str.split("").reverse().join("");
+    return str.split("").reverse().join("");
     
     // METHOD 2:
     
@@ -37,23 +37,23 @@
     // METHOD 6:
 
     // return str.split("").reduce((revString, char) => char + revString, "");
-// }
+}
 
 // CHALLENGE 2: Validate a palindrome.
-// function isPalindrome(str){
+function isPalindrome(str){
     // METHOD 1:
 
-    // const getPalindrome = str.split("").reverse().join("");
-    // return getPalindrome == str;
-// }
+    const getPalindrome = str.split("").reverse().join("");
+    return getPalindrome == str;
+}
 
 // CHALLENGE 3: Reverse an integer.
-// function reverseInt(int){
-//     return parseInt(int.toString().split("").reverse().join("")) * Math.sign(int);
-// }
+function reverseInt(int){
+    return parseInt(int.toString().split("").reverse().join("")) * Math.sign(int);
+}
 
 // CHALLENGE 4: Capitalize letters.
-// function capitalizeLetters(str){
+function capitalizeLetters(str){
     // METHOD 1:
 
     // const strArray = str.toLowerCase().split(" ");
@@ -64,56 +64,141 @@
 
     // METHOD 2:
 
-    // return str.toLowerCase().split(" ").map(word => word[0].toUpperCase() + word.substring(1)).join(" ");
+    return str.toLowerCase().split(" ").map(word => word[0].toUpperCase() + word.substring(1)).join(" ");
 
     // METHOD 3:
 
     // return str.replace(/\b[a-z]/gi, char => {
     //     return char.toUpperCase();
     // })
-// }
+}
 
 // CHALLENGE 5: Max Characters in a string. ERROR(??)
-// function maxCharacter(str) {
-//     const charMap = {};
-//     let maxNumber = 0;
-//     let maxChar = "";
+function maxCharacter(str) {
+    const charMap = {};
+    let maxNumber = 0;
+    let maxChar = "";
 
-//     str.split("").forEach(function(char) {
-//         if(charMap[char]){
-//             charMap[char]++;
-//         } else {
-//             charMap[char] = 1;
-//         }
-//     });
+    str.split("").forEach(function(char) {
+        if(charMap[char]){
+            charMap[char]++;
+        } else {
+            charMap[char] = 1;
+        }
+    });
 
-//     for(let char in charMap){
-//         if(charMap[char] > maxNumber){
-//             maxNum = charMap[char];
-//             maxChar = char;
-//         }
-//     }
-//     return maxChar;
-// }
+    for(let char in charMap){
+        if(charMap[char] > maxNumber){
+            maxNum = charMap[char];
+            maxChar = char;
+        }
+    }
+    return maxChar;
+}
 
 // // CHALLENGE 6: Fizzbuzz
-// function fizzbuzz(){
-//     for(let i = 1; i <= 100; i++){
-//         if(i % 15 === 0){
-//             console.log("FizzBuzz");
-//         }
-//         else if(i % 3 === 0) {
-//             console.log("Fizz")
-//         } else if (i % 5 === 0) {
-//             console.log("Buzz")
-//         } else {
-//             console.log(i);
-//         }
-//     }
-// }
+function fizzbuzz(){
+    for(let i = 1; i <= 100; i++){
+        if(i % 15 === 0){
+            console.log("FizzBuzz");
+        }
+        else if(i % 3 === 0) {
+            console.log("Fizz")
+        } else if (i % 5 === 0) {
+            console.log("Buzz")
+        } else {
+            console.log(i);
+        }
+    }
+}
 
-// CHALLENGE 7:
+// CHALLENGE 7: Longest word. If theres only one, return as a string, otherwise, return an array of the words.
+function longestWord(sen){
+    // Turn the sentence into lower case letters and use RegEx(regular expression) to get rid of the ",", reading only letters ranging from a-z and numbers 0-9.
+    const wordArr = sen.toLowerCase().match(/[a-z0-9]+/g);
 
+    // Sort by length
+    const sorted = wordArr.sort((a,b) => b.length - a.length);
 
-const output = fizzbuzz();
+    // If multiple words, put into an array.
+    const longestWordArr = sorted.filter(word =>{
+        return word.length === sorted[0].length;
+    })
+
+    // If theres only one longest word, return a string
+    if(longestWordArr.length === 1) {
+        return longestWordArr[0];
+    } else {
+        return longestWordArr;
+    }
+}
+
+// CHALLENGE 8: Chunking an array - Split an array into chunked arrays of specified length.
+function chunkArray(array, len) {
+    // METHOD 1:
+
+    // Initialize an empty array.
+    const chunkedArray = [];
+    // Index
+    let i = 0;
+
+    while( i < array.length) {
+        chunkedArray.push(array.slice(i, i + len));
+        i += 2;
+    }
+    return chunkedArray;
+
+    // METHOD 2 (way more complicated...):
+
+    // const chunkedArray = [];
+
+    // array.forEach(val => {
+    //     const last = chunkedArray[chunkedArray.length - 1];
+
+    //     if(!last || last.length === len) {
+    //         chunkedArray.push([val]);
+    //     } else {
+    //         last.push(val)
+    //     }
+    // })
+
+    // return chunkedArray;
+}
+// CHALLENGE 9: Flatten array. Take an array of arrays and flatten them into a single array.
+function flattenArray(array){
+    // METHOD 1:
+    // return array.reduce((a,b) => a.concat(b));
+
+    // METHOD 2:
+    // return [].concat.apply([], array);
+
+    // METHOD 3:
+    return [].concat(...array);
+}
+
+// CHALLENGE 10: Anagrams - Return true if anagram and false if not.
+function isAnagram(str1, str2){
+    // Returns true if they match, returns false if they dont.
+    return formatStr(str1) === formatStr(str2);
+}
+function formatStr(str) {
+    return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
+}
+
+// CHALLENGE 11: Letter changes - Change every letter of the string to the one that follows it and capitalize the vowels.
+function letterChange(str) {
+    let newString = str.toLowerCase().replace(/[a-z]/gi, char =>{
+        if(char === "z" || char === "Z") {
+            return "a"
+        } else {
+            return String.fromCharCode(char.charCodeAt() + 1)
+        }
+    });
+
+    newString = newString.replace(/a|e|i|o|u/gi, vowel =>{
+        return vowel.toUpperCase();
+    })
+    return newString;
+}
+const output =  letterChange("Hello There");
 console.log(output);
